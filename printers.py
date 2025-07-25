e00af1c7b01c1fce220366bab2651e7184cdd083
ichichkine <ichichkine@deltacontrols.com>
2025-07-24 23:38:52 -0700

    gdb: dynarray pretty printer

--- printers.py
+++ printers.py
@@ -1,18 +1,28 @@
 import gdb
+import gdb.printing
 
-class Widget:
+class DynarrayPrinter:
+    """Pretty printer for dynarray<T>"""
     def __init__(self, val):
         self.val = val
+        self.data = val['data_']
+        self.size = int(val['size_'])
+        self.capacity = int(val['capacity_'])
 
     def to_string(self):
-        # return str(self.val)
-        return f"Widget(path="
+        return f"dynarray(size={self.size}, capacity={self.capacity}, data_={self.data})"
 
-def lookup_type(val):
-    # print(f"lookup_type called with {val.type}")
-    # if str(val.type) == 'Widget':
-    #     return Widget(val)
-    return None
+    def children(self):
+        for i in range(self.size):
+            yield f"[{i}]", (self.data + i).dereference()
+
+    def display_hint(self):
+        return 'array'
+
+def build_dynarray_printer():
+    pp = gdb.printing.RegexpCollectionPrettyPrinter("dynarray")
+    pp.add_printer("dynarray", "^dynarray<.*>$", DynarrayPrinter)
+    return pp
 
 def register_printers(objfile):
-    gdb.printing.register_pretty_printer(objfile, lookup_type)
+    gdb.printing.register_pretty_printer(objfile, build_dynarray_printer())
